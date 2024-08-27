417368de06fe07d0cf7b8cb2572fcaedbcd6aae0
masakifujiwara1 <fmasaki2020@gmail.com>
2024-08-27 16:06:44 +0900

    Add windows version

---
+++ run_win.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+eval "docker container run \
+--network host \
+-it \
+--name my-cudagl \
+-e DISPLAY=$DISPLAY \
+-e PULSE_SERVER=$PULSE_SERVER \
+-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+-e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+-v $PWD/docker_share:/home/host_files \
+--privileged \
+-v /dev:/dev \
+-v /mnt/wslg:/mnt/wslg \
+--env="XAUTHORITY=$XAUTH" \
+-v "$XAUTH:$XAUTH" \
+--env="QT_X11_NO_MITSHM=1" \
+--ipc=host \
+masakifujiwara1/cudagl:11.3.0-devel-ubuntu20.04-torch"
